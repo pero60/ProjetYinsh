df29f5a4ba848b3f3a3ecb4d01bd71689bca3dd5
tperraul <thomas.perrault720@gmail.com>
2013-10-21 16:33:06 +0200

    new base

--- ExeptionYinsh.java
+++ ExeptionYinsh.java
@@ -1,12 +1,11 @@
 
 public class ExeptionYinsh extends Exception {
 
-	public ExeptionYinsh() {
-		
-	}
-	
-	public void MauvaiseCoordonnée()
-	{
-		System.out.println("les coordonnées saisient sont incorrects");
-	}
+    public ExeptionYinsh() {
+
+    }
+
+    public void MauvaiseCoordonnée() {
+        System.out.println("les coordonnées saisient sont incorrects");
+    }
 }
